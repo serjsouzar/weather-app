05322f1fde42f5b27970929c48f13ef593cc0935
Sergio Souza <serjsouza93@gmail.com>
2023-10-20 17:00:31 -0300

    search working

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 import search_icon from '../../assets/search.png'
@@ -8,33 +8,58 @@ import wind_icon from '../../assets/wind.png'
 
 const WeatherApp = () => {
 
-  console.log(process.env.REACT_APP_OPEN_WEATHER_KEY)
+  const api_key = process.env.REACT_APP_OPEN_WEATHER_KEY
+
+  const [humidity, setHumidity] = useState("64")
+  const [wind, setWind] = useState("18")
+  const [city, setCity] = useState("London")
+  const [temp, setTemp] = useState("25")
+
+
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput")
+    if(element[0].value === ""){
+      return 0
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+    const response = await fetch(url)
+    .then((data) => data.json())
+    .then((data) => {
+      setHumidity(data.main.humidity)
+      setWind(data.wind.speed)
+      setCity(data.name)
+      setTemp(data.main.temp)
+    })
+    
+    return response
+  }
+
 
   return (
     <div className='container'>
       <div className='top-bar'>
-        <input type="text" className="searchbar" placeholder='Search city'/>
-        <div className='search-icon'>
+        <input type="text" className="cityInput" placeholder='Search city'/>
+        <div className='search-icon' onClick={() => search()}>
         <img src={search_icon} alt=''/>
         </div>
       </div>
       <div className='weather-image'>
         <img src={cloud_icon} alt=''/>
       </div>
-      <div className='weather-temp'>24°c</div>
-      <div className='weather-location'>London</div>
+      <div className='weather-temp'>{temp}°c</div>
+      <div className='weather-location'>{city}</div>
       <div className='data-container'>
         <div className="element">
           <img src={humidity_icon} alt="" className="icon" />
           <div className='data'>
-            <div className='humidity-percent'>64%</div>
+            <div className='humidity-percent'>{humidity}%</div>
             <div className='text'>Humidity</div>
           </div>
         </div>
         <div className="element">
           <img src={wind_icon} alt="" className="icon" />
           <div className='data'>
-            <div className='humidity-percent'>18 km/h</div>
+            <div className='wind-rate'>{wind} km/h</div>
             <div className='text'>Wind Speed</div>
           </div>
         </div>
